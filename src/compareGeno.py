e5751605e136bea70913e23cb0e91e689e40d153
tobiasrausch <rauschtobi@gmail.com>
2015-10-10 14:45:51 +0200

    added genotype comparison

---
+++ src/compareGeno.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+import csv
+import argparse
+import numpy
+
+# Parse command line
+parser = argparse.ArgumentParser(description='Genotype comparison')
+parser.add_argument('-t', '--table', metavar='svfdr.txt', required=True, dest='tenxTable', help='genotype table (required)')
+args = parser.parse_args()
+
+geno = numpy.zeros((4, 4), dtype=numpy.int32)
+if args.tenxTable:
+    with open(args.tenxTable) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter='\t')
+        for row in reader:
+            geno[int(row['genotype']) + 1, int(row['calledgenotype']) + 1] += 1
+
+for i in range(1, 4):
+    if sum(geno[1:, i]) != 0:
+        discordance = (float(sum(geno[1:, i])) - float(geno[i, i]))/float(sum(geno[1:, i]))
+        discordance = numpy.round(discordance, decimals=4)
+        print("Genotype " + str(i-1) + "discordance: ", discordance)
+
+# Print genotype confusion matrix
+print("GT\tNA", end="")
+for i in range(3):
+    print("\t" + str(i), end="")
+print()
+for i in range(4):
+    for j in range(4):
+        if j != 0:
+            print("\t", end="")
+        else:
+            if i:
+                print(str(i-1) + "\t", end="")
+            else:
+                print("NA\t", end="")
+        print(geno[i, j], end="")
+    print()
+
+
--- src/svfdr.cpp
+++ src/svfdr.cpp
@@ -22,17 +22,35 @@ Contact: Tobias Rausch (rausch@embl.de)
 */
 
 #include <iostream>
+#include <sstream>
 #include <set>
+#include <map>
 #include <vector>
 #include <algorithm>
 #include <htslib/vcf.h>
 #include <htslib/sam.h>
+#include <math.h> 
+#include <stdio.h>
 
-inline bool splitPoint(bam1_t* rec, int32_t& split) {
+inline bool splitPoint(bam1_t* rec, int32_t& clipSize, int32_t& split, unsigned int qualCut) {
+  // Check for single soft-clip
+  unsigned int numSoftClip = 0;
+  uint32_t* cigar = bam_get_cigar(rec);
+  for (unsigned int i = 0; i < rec->core.n_cigar; ++i)
+    if (bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) ++numSoftClip;
+  if (numSoftClip != 1) return false;
+
+  // Get quality vector
+  typedef std::vector<uint8_t> TQuality;
+  TQuality quality;
+  quality.resize(rec->core.l_qseq);
+  uint8_t* qualptr = bam_get_qual(rec);
+  for (int i = 0; i < rec->core.l_qseq; ++i) quality[i] = qualptr[i];
+  
+  // Get soft-clips
   uint32_t alen = 0;
   uint32_t lastIns = 0;
-  uint32_t numSoftClip = 0;
-  uint32_t* cigar = bam_get_cigar(rec);
+  unsigned int meanQuality = 0;
   for (unsigned int i = 0; i < rec->core.n_cigar; ++i) {
     if (bam_cigar_op(cigar[i]) == BAM_CMATCH) {
       alen += bam_cigar_oplen(cigar[i]) + lastIns;
@@ -40,11 +58,14 @@ inline bool splitPoint(bam1_t* rec, int32_t& split) {
     } else if (bam_cigar_op(cigar[i]) == BAM_CINS) {
       lastIns = bam_cigar_oplen(cigar[i]);   // Only add if followed by 'M'
     } else if (bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) {
-      ++numSoftClip;
+      clipSize = bam_cigar_oplen(cigar[i]);
       split = rec->core.pos + alen;
+      unsigned int qualSum = 0;
+      for(unsigned int i = alen; i < (alen+clipSize); ++i) qualSum += quality[i];
+      meanQuality = qualSum / clipSize;
     }
   }
-  return (numSoftClip == 1);
+  return (meanQuality>=qualCut);
 }
 
 
@@ -71,7 +92,8 @@ int main(int argc, char **argv) {
 
   // Parameter
   std::string sampleName = std::string(argv[1]);
-  int32_t readLen = 100;
+  int32_t readLen = 150;
+  int32_t bpwindow = 25;
 
   // Load bcf file
   htsFile* ifile = bcf_open(argv[2], "r");
@@ -101,34 +123,59 @@ int main(int argc, char **argv) {
       break;
     }
   }
+  std::cout << "chr\tstart\tend\tid\tsize\tsvtype\thaplotype\tgenotype\tphasedblockid\tsrH1\tcovH1\tsrH2\tcovH2\tcalledhaplotype\tcalledgenotype" << std::endl;
   while (bcf_read(ifile, hdr, rec) == 0) {
     bcf_unpack(rec, BCF_UN_ALL);
     bcf_get_format_int32(hdr, rec, "GT", &gt, &ngt);
     if ((bcf_gt_allele(gt[sampleIndex*2]) != -1) && (bcf_gt_allele(gt[sampleIndex*2 + 1]) != -1)) {
       int gt_type = bcf_gt_allele(gt[sampleIndex*2]) + bcf_gt_allele(gt[sampleIndex*2 + 1]);
-      if (gt_type != 0) {
+      std::string gtval;
+      if (bcf_gt_is_phased(bcf_alleles2gt(bcf_gt_allele(gt[sampleIndex*2]), bcf_gt_allele(gt[sampleIndex*2 + 1])))) {
+	std::ostringstream s;
+	s << bcf_gt_allele(gt[sampleIndex*2]) << '|' << bcf_gt_allele(gt[sampleIndex*2 + 1]);
+	gtval = s.str();
+      } else {
+	std::ostringstream s;
+	s << bcf_gt_allele(gt[sampleIndex*2]) << '/' << bcf_gt_allele(gt[sampleIndex*2 + 1]);
+	gtval = s.str();
+      }
+      if (gt_type != -1) {
 	bcf_get_info_string(hdr, rec, "SVTYPE", &svt, &nsvt);
 	if (std::string(svt) == "DEL") {
 	  bcf_get_info_int32(hdr, rec, "END", &svend, &nsvend);
 
-	  std::string uniquePS = "Default";
+	  // Phased block identifier
+	  std::string uniquePS = "None";
+	  
+	  // SV region
 	  std::string chrName = bcf_hdr_id2name(hdr, rec->rid);
 	  if (chrName.substr(0,3) != "chr") chrName = std::string("chr").append(chrName);
+	  if ((chrName == "chrX") || (chrName == "chrY")) continue;   // Only autosomes
+	    
 	  int32_t regionChr = bam_name2id(hd, chrName.c_str());
 	  int32_t regionStart = std::max(0, rec->pos - readLen);
 	  int32_t regionEnd = (*svend) + readLen;
 	  int32_t regionSize = regionEnd - regionStart;
+
+	  // Split-read information
 	  typedef std::vector<uint32_t> TSplitPoints;
 	  TSplitPoints sppH1;
 	  sppH1.resize(regionSize, 0);
 	  TSplitPoints sppH2;
 	  sppH2.resize(regionSize, 0);
+
+	  // Base pair counts
 	  typedef std::vector<uint32_t> TBPCounts;
 	  TBPCounts bpH1;
 	  bpH1.resize(regionSize, 0);
 	  TBPCounts bpH2;
 	  bpH2.resize(regionSize, 0);
+	  std::size_t bpCountH1 = 0;
+	  std::size_t bpCountH2 = 0;
 
+	  // Initial breakpoints
+	  int32_t svStart = rec->pos - regionStart;
+	  int32_t svEnd = (*svend) - regionStart;
 	  
 	  hts_itr_t* iter = sam_itr_queryi(idx, regionChr, regionStart, regionEnd);
 	  bam1_t* r = bam_init1();
@@ -149,22 +196,29 @@ int main(int argc, char **argv) {
 		  singlePhasedBlock = false;
 		  break;
 		}
-	      } else uniquePS = rPS;
+	      } else {
+		psSet = true;
+		uniquePS = rPS;
+	      }
 
 	      // Get the haplotype
 	      uint8_t* hpptr = bam_aux_get(r, "HP");
 	      if (hpptr) {
 		int hap = bam_aux2i(hpptr);
+
+		// Store base-count
+		if (hap==1) addBpCounts(r, regionStart, regionEnd, bpH1);
+		else addBpCounts(r, regionStart, regionEnd, bpH2);
+		
+		// Store split-read
 		int32_t split = -1;
-		if (splitPoint(r, split)) {
+		int32_t clipSize = -1;
+		if (splitPoint(r, clipSize, split, 20)) {
 		  if ((split >= regionStart) && (split < regionEnd)) {
 		    split -= regionStart;
-		    if (hap==1) {
-		      ++sppH1[split];
-		      addBpCounts(r, regionStart, regionEnd, bpH1);
-		    } else {
-		      ++sppH2[split];
-		      addBpCounts(r, regionStart, regionEnd, bpH2);
+		    if (clipSize > (int) (log10(r->core.l_qseq) * 10)) {
+		      if (hap==1) ++sppH1[split];
+		      else ++sppH2[split];
 		    }
 		  }
 		}
@@ -175,11 +229,87 @@ int main(int argc, char **argv) {
 	  hts_itr_destroy(iter);
 
 	  if (singlePhasedBlock) {
-	    for(std::size_t i = 0; i<sppH1.size(); ++i) {
-	      std::cout << sppH1[i] << ',' << sppH2[i] << "\t" << bpH1[i] << ',' << bpH2[i] << std::endl;
+	    // Search refined breakpoint
+	    int32_t svstartbeg = std::max(0, (rec->pos - regionStart) - bpwindow);
+	    int32_t svstartend = (rec->pos - regionStart) + bpwindow;
+	    int32_t svendbeg = ((*svend) - regionStart) - bpwindow;
+	    int32_t svendend = ((*svend) - regionStart) + bpwindow;
+	    if (svstartend > svendbeg) {
+	      int32_t mid = (svendbeg + svstartend) / 2;
+	      svstartend = mid - 1;
+	      svendbeg = mid;
+	    }
+
+	    // Which haplotype to search for split reads?
+	    for(std::size_t i = svStart + 1; i < (std::size_t) svEnd; ++i) {
+	      bpCountH1 += bpH1[i];
+	      bpCountH2 += bpH2[i];
+	    }
+	    TSplitPoints spp = sppH1;
+	    if (bpCountH1 > bpCountH2) spp = sppH2;
+	    uint32_t sC = spp[svStart];
+	    for(std::size_t i = svstartbeg; i < (std::size_t) svstartend; ++i) {
+	      if (spp[i] > sC) {
+		svStart = i;
+		sC = spp[i];
+	      }
+	    }
+	    sC = spp[svEnd];
+	    for(std::size_t i = svendbeg; i < (std::size_t) svendend; ++i) {
+	      if (spp[i] > sC) {
+		svEnd = i;
+		sC = spp[i];
+	      }
+	    }
+
+	    // Re-calculate bp counts based on refined SV breakpoints
+	    bpCountH1 = 0;
+	    bpCountH2 = 0;
+	    for(std::size_t i = svStart + 1; i < (std::size_t) svEnd; ++i) {
+	      bpCountH1 += bpH1[i];
+	      bpCountH2 += bpH2[i];
+	    }
+	  } else uniquePS = "None";
+	  // Calculate coverage
+	  std::size_t svSize = svEnd - svStart;
+	  double covH1 = (double) bpCountH1 / (double) svSize;
+	  double covH2 = (double) bpCountH2 / (double) svSize;
+
+	  // SR support
+	  uint32_t srH1 = sppH1[svStart] + sppH1[svEnd];
+	  uint32_t srH2 = sppH2[svStart] + sppH2[svEnd];
+
+	  // Call genotype
+	  std::string gtstr = "None";
+	  int gtcalled = -1;
+	  if (uniquePS != "None") {
+	    if (covH1 < 1) {
+	      if (covH2 < 1) {
+		if ((srH1 >= 2) && (srH2 >= 2)) {
+		  gtstr = "1/1";
+		  gtcalled = 2;
+		}
+	      } else {
+		if ((srH1 >= 2) && (srH2 == 0)) { 
+		    gtstr = "1|0";
+		    gtcalled = 1;
+		}
+	      }
+	    } else {
+	      if (covH2 < 1) {
+		if ((srH1 == 0) && (srH2 >= 2)) {
+		  gtstr = "0|1";
+		  gtcalled = 1;
+		}
+	      } else {
+		if ((srH1 == 0) && (srH2 == 0)) {
+		  gtstr = "0/0";
+		  gtcalled = 0;
+		}
+	      }
 	    }
 	  }
-	  exit(-1);
+	  std::cout << bcf_hdr_id2name(hdr, rec->rid) << "\t" << (rec->pos + 1) << "\t" << *svend << "\t" << rec->d.id << "\t" << ((*svend) - rec->pos) << "\t" << svt << "\t" << gtval << "\t" << gt_type << "\t" << uniquePS << "\t" << srH1 << "\t" << covH1 << "\t" << srH2 << "\t" << covH2  << "\t" << gtstr << "\t" << gtcalled << std::endl;
 	}
       }
     }
