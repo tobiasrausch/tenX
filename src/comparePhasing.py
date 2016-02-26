ba7a4a200130aa7d78c3afff4e3d1ff9463f5055
tobiasrausch <rauschtobi@gmail.com>
2016-02-26 11:55:09 +0100

    changes to phasing

--- src/comparePhasing.py
+++ src/comparePhasing.py
@@ -3,42 +3,48 @@
 from __future__ import print_function
 import csv
 import argparse
-import numpy
+import collections
 
 # Parse command line
 parser = argparse.ArgumentParser(description='Phasing comparison')
 parser.add_argument('-t', '--table', metavar='svgeno.txt', required=True, dest='tenxTable', help='genotype table (required)')
 args = parser.parse_args()
 
-geno = numpy.zeros((4, 4), dtype=numpy.int32)
 if args.tenxTable:
     with open(args.tenxTable) as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t')
         psID = ""
-        correctPhase = 0
-        total = 0
+        total = collections.Counter()
+        correctPhase = collections.Counter()
         haplotypeTotal = 0
         haplotypeCooccurrence = 0
         for row in reader:
-            if (int(row['genotype']) == 1) and (int(row['calledgenotype']) == 1):
+            if ('genotype' not in row.keys()) or ((int(row['genotype']) == 1) and (int(row['calledgenotype']) == 1)):
                 hGiven = None
                 if '|' in row['haplotype']:
                     hGiven = [int(i) for i in row['haplotype'].split('|')]
                 hCalled = [int(i) for i in row['calledhaplotype'].split('|')]
                 if psID != row['phasedblockid']:
                     psID = row['phasedblockid']
-                    oldHGiven = hGiven
-                    oldHCalled = hCalled
+                    if '/' in row['haplotype']:
+                        hGiven = [int(i) for i in row['haplotype'].split('/')]
                 else:
                     if hGiven is not None:
-                        total += 1
+                        total[row['chr']] += 1
                         if ((hGiven[0] == hCalled[0]) and (oldHGiven[0] == oldHCalled[0])) or ((hGiven[0] == hCalled[1]) and (oldHGiven[0] == oldHCalled[1])):
-                            correctPhase += 1
+                            correctPhase[row['chr']] += 1
                     haplotypeTotal += 1
                     if hCalled[0] == oldHCalled[0]:
                         haplotypeCooccurrence += 1
-        if total:
-            print("Phasing accuracy: ", float(correctPhase)/float(total), " ( #n =", total, ")")
+                oldHGiven = hGiven
+                oldHCalled = hCalled
+        for c in sorted(total.keys()):
+            if total[c]:
+                print("Phasing accuracy, ", c, ": ", float(correctPhase[c])/float(total[c]), " ( #n =", total[c], ")")
+        genomictotal = sum(total.values())
+        genomicCorrectPhase = sum(correctPhase.values())
+        if genomictotal:
+            print("Phasing accuracy: ", float(genomicCorrectPhase)/float(genomictotal), " ( #n =", genomictotal, ")")
         print("Haplotype co-occurrence: ", float(haplotypeCooccurrence)/float(haplotypeTotal), " ( #n =", haplotypeTotal, ")")
 
 
